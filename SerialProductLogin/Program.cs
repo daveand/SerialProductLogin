100f9038055fda9060841353671ddaf6f5de65a8
David Andersson <david.o.andersson@se.atlascopco.com>
2019-06-03 16:30:55 +0200

    Initial commit

---
+++ SerialProductLogin/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SerialProductLogin
+{
+    class Program
+    {
+        static bool isArticle;
+
+        static List<string> articles = new List<string>(ConfigurationManager.AppSettings["Articles"].Split(new char[] { ';' }));
+
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                SerialComm s = new SerialComm();
+
+                // Declare variables
+                //string scannedValue;
+                string scan1 = "";
+                string scan2;
+                string product;
+
+                while (scan1 != "error")
+                {
+                    scan1 = s.Read();
+
+                    // Check if first scan matched any article number
+                    isArticle = GetArticle(scan1);
+
+                    if (isArticle && scan1 != "error")
+                    {
+                        Console.WriteLine($"Scanned value {scan1} is an article. Scan product ID.");
+                        scan2 = s.Read();
+                        product = scan2 + scan1;
+                        s.Send(product);
+                        isArticle = false;
+                    }
+                    else if (scan1 != "error")
+                    {
+                        Console.WriteLine("Scanned value is not an article. Sending first scan.");
+                        s.Send(scan1);
+                        isArticle = false;
+                    }
+
+                }
+
+                Console.WriteLine("Is another application using the COM-port?\nPress any key to try again...");
+                Console.ReadLine();
+
+            }
+
+        }
+
+        static bool GetArticle(string data)
+        {
+            foreach (string x in articles)
+            {
+                if (x.Equals(data))
+                {
+                    isArticle = true;
+                }
+            }
+            return isArticle;
+        }
+    }
+}
---
+++ SerialProductLogin/SerialComm.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Configuration;
+using System.IO.Ports;
+using System.Threading;
+
+namespace SerialProductLogin
+{
+    public class SerialComm
+    {
+        static string comPortIn = ConfigurationManager.AppSettings.Get("ComPortIn");
+        static string comPortOut = ConfigurationManager.AppSettings.Get("ComPortOut");
+
+        public string terminatorIn1 = ConfigurationManager.AppSettings.Get("TerminatorIn1");
+        public string terminatorIn2 = ConfigurationManager.AppSettings.Get("TerminatorIn2");
+        public string terminatorOut1 = ConfigurationManager.AppSettings.Get("TerminatorOut1");
+        public string terminatorOut2 = ConfigurationManager.AppSettings.Get("TerminatorOut2");
+
+        SerialPort serialIn = new SerialPort(comPortIn);
+        SerialPort serialOut = new SerialPort(comPortOut);
+
+        public SerialComm()
+        {
+
+
+            serialIn.BaudRate = 9600;
+            serialIn.Parity = Parity.None;
+            serialIn.StopBits = StopBits.One;
+            serialIn.DataBits = 8;
+            serialIn.Handshake = Handshake.None;
+            serialIn.RtsEnable = true;
+
+
+
+            serialOut.BaudRate = 9600;
+            serialOut.Parity = Parity.None;
+            serialOut.StopBits = StopBits.One;
+            serialOut.DataBits = 8;
+            serialOut.Handshake = Handshake.None;
+            serialOut.RtsEnable = true;
+            serialOut.WriteTimeout = 5000;
+
+
+        }
+
+        public string Read()
+        {
+
+            string indata;
+            //Console.WriteLine(terminatorIn1 + terminatorIn2);
+            try
+            {
+                serialIn.Open();
+                Console.WriteLine("Waiting for barcode scan...");
+                indata = serialIn.ReadTo(terminatorIn1 + terminatorIn2);
+                Console.WriteLine($"Received from {serialIn.PortName}: {indata}");
+                return indata;
+
+            }
+            catch (Exception ex)
+            {
+                string returnError = "error";
+                Console.WriteLine(ex.Message);
+                return returnError;
+            }
+            finally
+            {
+                serialIn.Close();
+            }
+        }
+
+        public void Send(string data)
+        {
+            serialOut.Open();
+
+
+            string outdata = data + terminatorOut1 + terminatorOut2;
+            try
+            {
+                serialOut.Write(outdata);
+                Console.WriteLine($"Sent data on {serialOut.PortName}: {outdata}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Could not send data on {serialOut.PortName}. {ex.Message} \n");
+            }
+            finally
+            {
+                serialOut.Close();
+            }
+
+
+
+        }
+
+    }
+
+}
